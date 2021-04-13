ba43fbb0c05942f16d829de0a2527174b5f47629
Travyon Stevens <56418099+Travyon@users.noreply.github.com>
2021-04-12 21:12:54 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,4 +1,3 @@
-//import the frame from the library
 
 
 import javax.swing.BorderFactory;
@@ -8,34 +7,22 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
 
-
-
-/* Make the GUI Object in the main method so when 
-   you press run it will make it with the constructor.
-*/
-
 public class GUI{
 
-
-
-    //Constructor method is where the objects will go.
     public GUI(){
-        //Frame
+       
         JFrame frame = new JFrame();
         
-        //Button
-        JButton button = new JButton("Click me");
+       JButton button = new JButton("Click me");
     
-        //Panel
+        
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
         panel.add(button);
 
         
 
-        
     
-        //frame
        
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,14 +32,9 @@ public class GUI{
 
     }
 
-    //Main method
+   
     public static void main(String[] args){
        
-       /* Running the main method will prompt the
-       constructor which, will create the object
-       */
-
-      //Constructor
       new GUI(); 
     }  
     
